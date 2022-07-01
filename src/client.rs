b33f65f77d6b8245dbca1cf64fbc359702eb1568
coinsccg <825076979@qq.com>
2022-07-01 17:50:34 +0800

    add call

--- src/client.rs
+++ src/client.rs
@@ -1,23 +1,33 @@
-use std::str::FromStr;
+#![allow(dead_code)]
+#![allow(unused_imports)]
 
+use std::fs::File;
+use std::io::prelude::*;
 use web3::{
     self,
     Web3,
     Error,
     transports,
+    contract::{Contract, tokens::Tokenizable, Options, Error as ContractError},
+    ethabi::{self, Contract as AbiContract, Error as AbiError},
     types::{
         BlockNumber,
         U64,
         H160,
-        U256
+        U256,
+        H256
     }
 };
 
-pub type Result<T=()> = std::result::Result<T, Error>;
+type Result<T> = std::result::Result<T, Error>;
+type FileResult<T=String> = std::result::Result<T, std::io::Error>;
+type CallResult<T> = std::result::Result<T, ContractError>;
+
+static ZERO_ADDRESS: &'static str = "0x0000000000000000000000000000000000000000";
 
 #[derive(Debug, Clone)]
 pub struct Client{
-    pub client: Web3<transports::Http>
+    client: Web3<transports::Http>
 }
 
 impl Client {
@@ -31,5 +41,24 @@ impl Client {
         // let addr = H160::from_str(account).unwrap();
         let balance = self.client.eth().balance(account.parse().unwrap(), Some(BlockNumber::Number(U64::from(1000u32)))).await?;
         Ok(balance)
+    }
+
+    pub fn load_contract(path: &str) -> FileResult<String>{
+        let mut file = File::open(path).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        Ok(content)
+    }
+
+    pub fn zero_address() -> H160 {
+        ZERO_ADDRESS.parse().unwrap()
+    }
+
+    pub async fn contract_call(&self, path: &str, from: &str, contract_addr: &str, func_name: &str, param: &str) -> CallResult<H256> {
+        let contract = Contract::from_json(self.client.eth(), contract_addr.parse().unwrap(), Self::load_contract(path).unwrap().as_bytes())?;
+        let mut options = Options::default();
+        options.gas = Some(U256::from(100000u64));
+        let hash = contract.call(func_name, param.to_string().into_token(), from.parse().unwrap(), options).await?;
+        Ok(hash)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,17 @@ use web3;
 #[tokio::main]
 async fn main() -> web3::Result<()> {
     let rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/";
-    let cc = client::Client::new(rpc)?;
-    let balance = cc.get_balance("0x7cD1CB03FAE64CBab525C3263DBeB821Afd64483").await?;
+    let client = client::Client::new(rpc)?;
+    let balance = client.get_balance("0x7cD1CB03FAE64CBab525C3263DBeB821Afd64483").await?;
+    let from = "0x7cD1CB03FAE64CBab525C3263DBeB821Afd64483";
+    let contract = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd";
+    let func_name = "balanceOf";
+    let param = "0x7cD1CB03FAE64CBab525C3263DBeB821Afd64483";
     println!("{:?}", balance);
+    match client.contract_call("src/json/abi.json", from, contract, func_name, param).await {
+        Ok(hash) => println!("{:?}", hash),
+        Err(err) => println!("{:?}", err)
+    }
+    
     Ok(())
 }
